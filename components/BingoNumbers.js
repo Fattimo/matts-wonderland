496ef8431b07fdf240ca124b93e9fb08935b482e
Fattimo <matt.chen522@gmail.com>
2022-02-16 01:17:29 -0500

    fonts and colors

--- components/BingoNumbers.js
+++ components/BingoNumbers.js
@@ -2,7 +2,7 @@ const BingoNumbers = ({ top, left, rowGap, colGap, font, nums }) => {
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', rowGap: `${rowGap}`, columnGap: `${colGap}`, justifyItems: 'center', alignItems: 'center' }}>
       {nums.map((i, index) => (
-        <div key={i} style={{ fontSize:`${font}`, width: "5vw", height: "5vw", textAlign:"center" }}>{index === 12 ? 'X' : i}</div>
+        <div key={i} style={{ fontSize:`${font}`, width: "5vw", height: "5vw", textAlign:"center", color: index===12 ? "#2c323e" : ''}}>{index === 12 ? 'X' : i}</div>
       ))}
     </div>
   )
